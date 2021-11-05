47d77c4e6bbe25a94a7307a6f07b2f84291d07fb
Marcos Albera <alberamarcos@hotmail.com>
2021-11-05 11:16:48 -0300

    Update AlbumService.java
    Comertario

--- Trabajo Practico/SpotifyService/src/main/java/com/spotify/SpotifyService/Service/AlbumService.java
+++ Trabajo Practico/SpotifyService/src/main/java/com/spotify/SpotifyService/Service/AlbumService.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AlbumService {
+    
+    //Ojo con esto, estas haciendo una inyeccion a si misma, deberias remover   -->
+    //@Autowired
+    //private AlbumService albumService; <--
+    
     @Autowired
     private AlbumService albumService;
 
